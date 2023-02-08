fce9f44dd058e5e864b3c79e1baea4842e2e8670
mddmustainbillah <19103007@iubat.edu>
2023-02-09 01:18:55 +0600

    done setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine Q package", 
+    author="mbillah", 
+    packages=find_packages(),
+    license="MIT"
+)
+
+
+# pip install -e . 
