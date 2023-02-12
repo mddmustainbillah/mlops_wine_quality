c71ae3530c8f6cd7f379484a8e4b5d2ef822899d
mddmustainbillah <19103007@iubat.edu>
2023-02-12 13:19:01 +0600

    web structure added

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    description="its a wine Q package", 
-    author="mbillah", 
+    description="its a wine Q package",
+    author="mbillah",
     packages=find_packages(),
     license="MIT"
 )
 
 
-# pip install -e . 
+# pip install -e .
